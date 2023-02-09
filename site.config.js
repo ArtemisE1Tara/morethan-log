18844d191ebb21511670516236e48063b282b5aa
ArtemisE1Tara <ahmedsecen2@gmail.com>
2023-02-08 22:06:31 -0500

    dsad

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: 'Ahmed Secen',
-    image: '/avatar.svg',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: '/avatar.png',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: 'frontend developer/student',
     bio: 'I develop everything(?).',
     email: 'ahmedsecen2@gmail.com',
