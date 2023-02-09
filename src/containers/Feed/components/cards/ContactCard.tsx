a4a2e2d839e3902706d7ecbf16eb99d266467979
ArtemisE1Tara <ahmedsecen2@gmail.com>
2023-02-08 20:32:47 -0500

    test

--- src/containers/Feed/components/cards/ContactCard.tsx
+++ src/containers/Feed/components/cards/ContactCard.tsx
@@ -10,7 +10,7 @@ import {
 const ContactCard: React.FC = () => {
   return (
     <>
-      <div className="p-1 mb-3 dark:text-white">💬 Contact</div>
+      <div className="p-1 mb-3 dark:text-white">Contact</div>
       <ul className="rounded-2xl bg-white dark:bg-zinc-700 p-1 flex flex-col">
         {CONFIG.profile.github && (
           <a
@@ -36,7 +36,7 @@ const ContactCard: React.FC = () => {
         )}
         {CONFIG.profile.email && (
           <a
-            href={`mailto:${CONFIG.profile.email}`}
+            href={`mailto:ahmedsecen2@gmail.com`}
             rel="noreferrer"
             target="_blank"
             className="overflow-hidden p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
